fe8928adebb66fa4ab4e93954fcf8e55aed5518a
blueberry20 <olgasales@gmail.com>
2018-06-12 23:18:35 -0400

    deploy to heroku

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import WeatherList from './components/weatherList';
 import CityWeatherDetail from './components/CityWeatherDetail';
 
 const API_KEY = "8f2ff7369e6685a359ab7782aaefbb38";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&APPID=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&APPID=${API_KEY}`;
 
 
 class App extends Component {
